e8bb024a2b3356f53eecc5c5b5611d5668431e35
Richard Boase <richardwboase@gmail.com>
2025-09-21 23:06:49 +0100

    Clean up header UI - remove redundant components and social links

--- components/EmailClient.tsx
+++ components/EmailClient.tsx
@@ -10,11 +10,9 @@ import { EmailPreview } from './email/EmailPreview';
 import { ComposeModal } from './email/ComposeModal';
 import { ConnectionBadge } from './ConnectionBadge';
 import { ConnectionsModal } from './ConnectionsModal';
-import { WalletManager } from './WalletManager';
 import { EmailListExchange } from './exchange/EmailListExchange';
 import { ListMarketplace } from './exchange/ListMarketplace';
 import { CreateListModal } from './exchange/CreateListModal';
-import './WalletManager.css';
 
 const EmailClient: React.FC = () => {
   const router = useRouter();
@@ -157,14 +155,6 @@ const EmailClient: React.FC = () => {
     <div className="email-client">
       {/* Sophisticated Header */}
       <header className="email-header">
-        <div className="header-actions-left">
-          <button className="icon-button">
-            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-            </svg>
-          </button>
-        </div>
-
         <div className="title-section">
           <div className="app-title-container">
             <div className="app-logo envelope-logo">
@@ -222,39 +212,11 @@ const EmailClient: React.FC = () => {
             </svg>
             <span>Compose</span>
           </button>
-
-          <WalletManager className="wallet-manager-header" />
           
           <ConnectionBadge
             connections={connections}
             onOpenModal={() => setShowConnectionsModal(true)}
           />
-
-          {/* Twitter/X Link */}
-          <a
-            href="https://x.com/bitcoin_email"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="twitter-link-header"
-            title="Follow us on X"
-          >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
-            </svg>
-          </a>
-
-          {/* GitHub Link */}
-          <a
-            href="https://github.com/bitcoin-apps-suite/bitcoin-email"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="github-link-header"
-            title="View on GitHub"
-          >
-            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-              <path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" />
-            </svg>
-          </a>
         </div>
       </header>
 
